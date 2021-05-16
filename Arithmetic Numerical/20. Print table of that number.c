f01397752fb2b9acfa8100f40c3ad6c9a67477e4
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-16 16:16:08 +0530

    Arithmetic Operation's in C

---
+++ Arithmetic Numerical/20. Print table of that number.c
@@ -0,0 +1,18 @@
+/* 20. Program to take number from user and print table of that number. */
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i, n;
+    clrscr();
+
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= 10; i++)
+        printf("%d × %d = %d\n", n, i, n*i);
+
+    getch();
+}
