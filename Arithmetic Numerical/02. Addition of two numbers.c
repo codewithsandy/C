486bd37ff9786ec0fcbc223cda23637315f58b51
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-16 16:02:00 +0530

    Arithmetic Operation's in C

---
+++ Arithmetic Numerical/02. Addition of two numbers.c
@@ -0,0 +1,16 @@
+/* 2. Program to assign values of two numbers and print their addition. */
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a=10,b=20;
+  clrscr();
+
+  int ans = a + b;
+
+  printf("Addition is : %d",ans);
+
+  getch();
+}
